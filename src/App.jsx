41a110a21ebc9c9ee35e5296f9f42d9de787f172
Diane <dianahporter@gmail.com>
2026-03-25 15:42:25 +0200

    feat: React-Router setup

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div> 
+      <h1 className='text-red-700'>My React Router App</h1> 
+    </div> 
+  );
+}
+
+export default App
